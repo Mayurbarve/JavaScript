f9399d82a281a5f09d6688c2ad323cf93d712d47
Mayura Dudhbarve <mayurbarve04@gmail.com>
2024-05-06 23:43:46 +0530

    object in javascript

---
+++ JavaScript-1/Object.js
@@ -0,0 +1,13 @@
+//Javascript Object 
+
+let obj = {
+    name:"mayura",
+    age:22,
+    "working area":"Sakhi & ",
+    fun: function(){
+        console.log("Her Eyes")
+    }
+}
+
+console.log(obj["working area"]);
+console.log(obj.fun());
