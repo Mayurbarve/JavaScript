e2690beaa638e38640a4403ec486d27a770e3277
Mayura Dudhbarve <mayurbarve04@gmail.com>
2024-05-09 18:47:36 +0530

    Maps And its Function

--- JavaScript-1/Map.js
+++ JavaScript-1/Map.js
@@ -1,1 +1,19 @@
 //map
+
+var map1 = new Map([[1, "one"],
+["Me", "Mayur"],
+["Num", [0, 1, 2, 3, 4]]]);
+
+console.log(map1);
+
+//get values using key
+console.log(map1.get(1));
+
+//Set value in set
+map1.set("You", "Sakshi")
+
+//get keys using For of
+var k = map1.keys();
+for (let key of k) {
+    console.log(key);
+}
